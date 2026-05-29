1405c95879f32bb80f13fc6ffd2f7c4e447f2259
jun0305-yoko <monokuma03051999@icloud.com>
2026-05-29 15:58:10 +0900

    Update test_excel.py

--- test_excel.py
+++ test_excel.py
@@ -4,3 +4,9 @@ df = pd.read_excel("sample.xlsx")
 
 for i, row in df.iterrows():
     assert row["金額"] >= 0, f"{i}行目 金額NG: {row['金額']}"
+
+def test_amount():
+    df = pd.read_excel("sample.xlsx")
+
+    for i, row in df.iterrows():
+        assert row["金額"] >= 0
